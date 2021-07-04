b99fd7c9409fb40088cb5e25cbbea29a239a36f7
Bhupendra SIngh <bhupender.infoshore@gmail.com>
2021-07-04 18:52:58 +0530

    updated

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function menu() {
+
+
+
+
+    var x = document.getElementById("menu1");
+
+    if (x.style.display == "block") {
+        x.style.display = "none";
+  
+    } else {
+        x.style.display = "block";
+
+    }
+
+}
